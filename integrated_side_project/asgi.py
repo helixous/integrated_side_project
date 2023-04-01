64817dc8effb6ddb83c1986c1fc5a6ab57c9c5a8
helixous <parksehoon94@gmail.com>
2023-04-01 17:13:57 +0900

    Initial commit

---
+++ integrated_side_project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for integrated_side_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "integrated_side_project.settings")
+
+application = get_asgi_application()
