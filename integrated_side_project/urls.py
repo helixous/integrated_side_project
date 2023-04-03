cdda6871cf4fd0dc2317434cad49fb54fd22b0de
helix <parksehoon94@gmail.com>
2023-04-03 09:55:35 +0900

    urls.py 업데이트

--- integrated_side_project/urls.py
+++ integrated_side_project/urls.py
@@ -13,9 +13,61 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+# 배포된 서버에서는
+# HTTPS 요청만을 처리하므로
+# API 문서에서 기본적으로 HTTPS로 라우팅되도록 해야한다.
+# 또한 로컬환경에서는 https를 적용할 수 없으므로
+# DEBUG 모드일때는 http만 서빙되도록 수정한다.
+class CustomSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        # 디버그모드
+        if settings.DEBUG:
+            schema.schemes = ["http", "https"]
+        # 배포모드
+        else:
+            schema.schemes = ["https", "http"]
+        return schema
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="론픽 API",
+        default_version='v1',
+        description="론픽 통합 백엔드 API 문서입니다.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="info@ronfic.com"),
+        license=openapi.License(name="Ronfic License"),
+    ),
+    generator_class=CustomSchemaGenerator,
+    validators=['flex', ],
+    public=True,
+    permission_classes=(permissions.IsAuthenticated,),
+)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
+
+# API문서 자동화 URL 매핑
+urlpatterns += [
+    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', login_required(schema_view.with_ui('swagger', cache_timeout=0)), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', login_required(schema_view.with_ui('redoc', cache_timeout=0)), name='schema-redoc'),
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
