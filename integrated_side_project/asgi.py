64fa0ccbed0897fdce495fc0f77d3e955b2e2266
helix <parksehoon94@gmail.com>
2023-04-04 20:35:06 +0900

    django channels 관련 asgi 세팅 업로드

--- integrated_side_project/asgi.py
+++ integrated_side_project/asgi.py
@@ -9,8 +9,31 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from chat import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "integrated_side_project.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        # (http->django views is added by default)
+        'http': get_asgi_application(),
+
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    routing.websocket_urlpatterns
+                )
+            )
+        ),
+        # 'websocket': AuthMiddlewareStack(
+        #     URLRouter(
+        #         routing.websocket_urlpatterns
+        #     )
+        # ),
+    }
+)
