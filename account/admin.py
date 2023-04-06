95dac382560943e407c5fbf8da45b2e070b8fa43
helix <parksehoon94@gmail.com>
2023-04-06 17:05:54 +0900

    관리자 Admin 추가

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from account.models import AdministrationUser
+
 
 # Register your models here.
+
+@admin.register(AdministrationUser)
+class AdministrationUserAdmin(UserAdmin):
+    pass
